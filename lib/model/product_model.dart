dfba31fba2624d6807cdf0991b32fed38b85777f
sajidh <sajidhshaji4@gmail.com>
2024-07-18 23:44:39 +0530

    model class created for product

---
+++ lib/model/product_model.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+class ProductModel {
+  int id;
+  String title;
+  int price;
+  String description;
+  String category;
+  String image;
+  Rating rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductModel.fromRawJson(String str) =>
+      ProductModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+      id: json["id"],
+      title: json["title"],
+      price: json["price"],
+      description: json["description"],
+      category: json["category"],
+      image: json["image"],
+      rating: Rating.fromJson(json["rating"]));
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": rating.toJson()
+      };
+}
+
+class Rating {
+  int rate;
+  int count;
+
+  Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  factory Rating.fromRawJson(String str) => Rating.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"],
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+      };
+}
